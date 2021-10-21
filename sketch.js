3fae1b1282174bdc4537def11bd9e0c616bc1043
Abhinav2008-lab <75878501+Abhinav2008-lab@users.noreply.github.com>
2021-10-21 06:18:31 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function preload() {
+  
+}
+
+function setup() {
+createCanvas(600, 200);
+
+//create a trex sprite
+
+  
+//create a ground sprite
+
+}
+
+function draw() {
+background(220);
+
+//jump when the space button is pressed
+
+}
